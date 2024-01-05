e5a3b985f15feeecda134b45be2fb79282ab95d8
M4Lets <m4lets@MacBook-M4Lets.local>
2024-01-06 01:21:48 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+import './/vars';
+
+console.log('1')
+
+//try async function
+// async function foo() {
+//     console.log('async working!')
+// }
+
---
+++ app/js/vars.js
@@ -0,0 +1,14 @@
+//global vars
+
+window.$ = (el) => document.querySelector(el);
+window.$$ = (el) => document.querySelectorAll(el);
+
+window.isMobile = () => {
+if(/Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent)) {
+return true;
+};
+return false;
+}
+
+  
+
